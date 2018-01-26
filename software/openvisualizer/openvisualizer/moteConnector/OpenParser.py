889368fcc61e95e054e39f5ca6500c59502224ba
Fabrice Theoleyre <theoleyre@unistra.fr>
2018-01-26 11:42:17 +0100

    logging for parserStat

--- software/openvisualizer/openvisualizer/moteConnector/OpenParser.py
+++ software/openvisualizer/openvisualizer/moteConnector/OpenParser.py
@@ -27,7 +27,8 @@ class OpenParser(Parser.Parser):
     SERFRAME_MOTE2PC_CRITICAL          = ParserIEC.ParserInfoErrorCritical.SEVERITY_CRITICAL
     SERFRAME_MOTE2PC_REQUEST           = ord('R')
     SERFRAME_MOTE2PC_SNIFFED_PACKET    = ord('P')
-    
+    SERFRAME_MOTE2PC_STAT              = ord('T')
+
     SERFRAME_PC2MOTE_SETDAGROOT        = ord('R')
     SERFRAME_PC2MOTE_DATA              = ord('D')
     SERFRAME_PC2MOTE_TRIGGERSERIALECHO = ord('S')
